bda21d028847cf698463254cd48f07cf53b6f8a4
katsu tanabe <katsu1@live.com>
2019-06-16 17:48:13 -0600

    timer added homework donegit add .git add .

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -45,6 +45,50 @@ for (i = 1; i <= total; i++) {
  return false;
 
 }
+var number = 60;
 
+    //  Variable that will hold our interval ID when we execute
+    //  the "run" function
+    var intervalId;
+    //  The run function sets an interval
+    //  that runs the decrement function once a second.
+    //  Clearing the intervalId prior to setting our new intervalId will not allow multiple instances.
+    function run() {
+      clearInterval(intervalId);
+      intervalId = setInterval(decrement, 1000);
+    }
+
+    //  The decrement function.
+    function decrement() {
+
+      //  Decrease number by one.
+      number--;
+
+      //  Show the number in the #show-number tag.
+      $("#show-number").html("<h2>" + number + "</h2>");
+
+
+      //  Once number hits zero...
+      if (number === 0) {
+
+        //  ...run the stop function.
+        stop();
+
+        //  Alert the user that time is up.
+        alert("Time Up!");
+      }
+    }
+     //  The stop function
+     function stop() {
+
+        //  Clears our intervalId
+        //  We just pass the name of the interval
+        //  to the clearInterval function.
+        clearInterval(intervalId);
+      }
+  
+      //  Execute the run function.
+      run();
+  
  
 
